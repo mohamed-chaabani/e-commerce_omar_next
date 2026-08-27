9f988c30f547da00ccab67c463c535a18bf78b08
Yassine_Chaabeni <chaabeniyassine76@gmail.com>
2026-08-27 12:10:01 +0100

    feat: add Google Search Console verification tag

--- app/layout.js
+++ app/layout.js
@@ -60,6 +60,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
+        <meta name="google-site-verification" content="MhuUOOM8Mrlbh0x-JGurKb-yQw36ZNWWIOzPggTQkMQ" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
